92261593ee0add0b6506a7d1844acb852b78e247
Akash0437 <akashbirajdar814@gmail.com>
2026-01-14 19:42:44 +0530

    cors fix

--- src/index.js
+++ src/index.js
@@ -8,7 +8,11 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "https://YOUR-FRONTEND.vercel.app",
+    credentials: true
+}));
+
 app.use(express.json());
 
 // Routes
